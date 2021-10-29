b3c6f5d37febf7ff5f3321db6c0f1997007cc4cf
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2021-10-29 12:18:34 +0200

    Add second subtask solution for barcodes assignment

--- barcodes/main.asm
+++ barcodes/main.asm
@@ -19,7 +19,7 @@ mov suma, 0;initial value for sum
 
 nastepny_znak:
     ;place for your code
-
+    mov al, byte ptr [edi + ecx]
     ;w AL znajduje sie znak
     mov ebx, 0; ebx będzie do znalezienia indeksu w tablic
 
@@ -52,8 +52,10 @@ nastepny_znak:
     mov ebx, offset znaki
     movzx eax, al
     ;skala 6 niemożliwa więc trzeba rozbić
+    ;ebx + 6*eax = (ebx + 4*eax) + eax + eax
     lea ebx, [ebx + 4 * eax]
-    lea ebx, [ebx + 2 * eax]
+    add ebx ,eax
+    add ebx ,eax
 
     ;ebx wskazuje na odpowiedni wiersz
     mov eax, 0; licznik bitów
@@ -80,5 +82,23 @@ nastepny_znak:
     ;w dx znajduje się gotowy word do odstawienia w edi
     mov word ptr [edi], dx
 
+    ;<ZADANIE 2>
+    ;ebx nadal wskazuje na początek wiersza
+    add ebx, 4;przeskakujemy 4 bajty
+    mov bx, word ptr [ebx]
+    add suma, bl
+    ;</ZADANIE 2>
+
     loop nastepny_znak
+
+    ;<ZADANIE 2>
+    ;w sumie znajduje się suma do podzielenia modulo 35
+    mov al, suma
+    mov ah, 0
+    div 35
+    mov dl, ah
+    ;w dl reszta
+    mov eax, offset suma
+    mov byte ptr [suma], dl
+    ;</ZADANIE 2>
 END
