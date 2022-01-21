3c203366ab9a6d71a351710834417b4b313c8902
Jakub Kuniszewski <jakubkuniszewski1@gmail.com>
2022-01-21 12:41:16 +0100

    Fix mask value in round task

--- round/main.asm
+++ round/main.asm
@@ -1,10 +1,10 @@
-;jesli czesc ulamkowa jest mniejsza niz 1/2 to zerujemy wszystkie bity ulamkowe
-;w przeciwnym razie ustawiamy je na 1
+;jeśli część ułamkowa jest mniejsza od 1/2 to nic nie robimy poza zerowaniem jej
+;w przeciwnym wypadku również zerujemy, ale zwiększamy część całkowitą o 1
 round PROC
 
 bt edx, 6
 jc roundUp
-and dl, 07Fh
+and dl, 00h
 jmp koniec
 roundUp:
 shr edx, 7
